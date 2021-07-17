7d9e9fba7dbec2bf5ad6b8366c50bb3d58dc30a4
kdm <kdm1jkmm@gmail.com>
2021-07-17 19:43:40 +0900

    Add react

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import Main from "./components/Main";
+import React from "react";
+import ReactDOM from "react-dom";
+
+window.React = React;
+ReactDOM.render(<Main />, document.getElementById("root"));
